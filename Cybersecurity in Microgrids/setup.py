6c5bda9cea3ee085d3b3e2fd91ed2c6acea35df4
Wyatt-Dyer <wyattjdyer@Gmail.com>
2024-09-12 13:52:54 +1000

    Initialised workspace

---
+++ Cybersecurity in Microgrids/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A Machine Learning Model that can identify and mitigate common cyber attacks in microgrids',
+    author='Wyatt Dyer',
+    license='',
+)
--- README.md
+++
@@ -1,2 +0,0 @@
-# Cybersecurity-in-Microgrids
-An AI model designed to identify and mitigate common cybersecurity attacks within a microgrid system.
